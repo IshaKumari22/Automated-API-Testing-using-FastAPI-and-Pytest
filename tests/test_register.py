9567e95cd446ac41d567e63f7b4329fdf2916c63
Isha Kumari <isha02740@gmail.com>
2026-01-07 12:23:30 +0530

    Add additional API test cases and stabilize CI workflow

--- tests/test_register.py
+++ tests/test_register.py
@@ -46,3 +46,58 @@ def test_login_user_not_found():
         }
     )
     assert response.json()["message"]=="User not found"
+
+
+
+def test_register_duplicate_user():
+    response1=httpx.post(
+        f"{BASE_URL}/register",
+        json={
+            "email":"duplicate@gmail.com",
+            "password":"123"
+        }
+
+    )
+    response2=httpx.post(
+        f"{BASE_URL}/register",
+        json={
+           
+            "email":"duplicate@gmail.com",
+            "password":"123"
+    
+        }
+    )
+    assert response2.json()["message"]=="User already exists"
+
+
+def test_login_missing_password():
+    response=httpx.post(
+        f"{BASE_URL}/login",
+        json={
+            "email":"test2gmail.com"
+        }
+    )
+
+    assert response.status_code==422
+
+def test_register_empty_email():
+    response = httpx.post(
+        f"{BASE_URL}/register",
+        json={
+            "email": "",
+            "password": "123"
+        }
+    )
+
+    assert response.status_code == 200
+def test_login_with_extra_field(register_user):
+    response = httpx.post(
+        f"{BASE_URL}/login",
+        json={
+            "email": "test@gmail.com",
+            "password": "123",
+            "age": 22
+        }
+    )
+
+    assert response.status_code == 200
